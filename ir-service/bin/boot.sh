7baa30d262cbe17fa8d5c812bcf8d57d1fe49cbd
Jeff Blaisdell <jeff.j.blaisdell@gmail.com>
2017-01-16 22:03:56 -0600

    Renamed to ir-service and patched bug seemingly with uwsgi debian init.d start up scripts

---
+++ ir-service/bin/boot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run confd to write out configuration files
+confd -onetime -backend env
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/ir-service /etc/nginx/sites-enabled/ir-service
+ln -s /etc/uwsgi/apps-available/ir-service.ini /etc/uwsgi/apps-enabled/ir-service.ini
+
+service uwsgi restart
+service nginx restart

8ec2d071c13881aa5365f5c430423c4be7f5832d
Jeff Blaisdell <jeff.j.blaisdell@gmail.com>
2017-02-05 15:40:02 -0600

    Get lirc running inside docker

--- ir-service/bin/boot.sh
+++ ir-service/bin/boot.sh
@@ -2,7 +2,9 @@
 
 # Setup Data Directory
 mkdir -p /var/data/ir-service/commands
+mkdir -p /var/run/lirc
 chown -R www-data:www-data /var/data/ir-service
+chown -R root:root /var/run/lirc
 
 # Run confd to write out configuration files
 confd -onetime -backend env
@@ -10,5 +12,6 @@ rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/ir-service /etc/nginx/sites-enabled/ir-service
 ln -s /etc/uwsgi/apps-available/ir-service.ini /etc/uwsgi/apps-enabled/ir-service.ini
 
+sudo /usr/sbin/lircd --driver=default --device=/dev/lirc0
 service uwsgi restart
 service nginx restart
